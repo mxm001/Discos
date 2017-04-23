fa400fe557af362e8e9a984ac432856a37f5a77e
maxi <maxi@maxi-PC>
2017-04-23 20:33:54 -0300

    Primer Commit Con src completo, TODO:test

---
+++ Discos/src/ar/edu/unlam/pb2/Disco.java
@@ -0,0 +1,41 @@
+package ar.edu.unlam.pb2;
+
+public class Disco {
+	
+	private Double radioExterior;
+	private Double radioInterior;
+	
+	//GET & SET
+	public Double getRadioExterior() {
+		return radioExterior;
+	}
+
+	public void setRadioExterior(Double radioExterior) {
+		this.radioExterior = radioExterior;
+	}
+
+
+	public Double getRadioInterior() {
+		return radioInterior;
+	}
+
+	public void setRadioInterior(Double radioInterior) {
+		this.radioInterior = radioInterior;
+	}
+	
+	//METODOS
+	public Double calcularPerimetroInterior()
+	{
+		return  2*Math.PI*this.radioInterior;
+	}
+	
+	public Double calcularPerimetroExterior()
+	{
+		return  2*Math.PI*this.radioExterior;
+	}
+	
+	public Double calcularSuperficie()
+	{
+		return Math.PI*Math.pow(this.radioExterior-this.radioInterior,2.00);
+	}
+}
